5d7a8feffe48fb9f31f741053b2f7c9c2e9f5bbf
Paulo Fernandes <phfernandespereira@gmail.com>
2023-05-12 12:24:29 -0300

    parametriza parmetro de pagamento por debito bradesco

--- lib/brcobranca/remessa/cnab400/bradesco.rb
+++ lib/brcobranca/remessa/cnab400/bradesco.rb
@@ -6,6 +6,7 @@ module Brcobranca
       class Bradesco < Brcobranca::Remessa::Cnab400::Base
         # codigo da empresa (informado pelo Bradesco no cadastramento)
         attr_accessor :codigo_empresa
+        attr_accessor :habilita_debito_automatico
 
         validates_presence_of :agencia, :conta_corrente, message: 'não pode estar em branco.'
         validates_presence_of :codigo_empresa, :sequencial_remessa,
@@ -94,7 +95,7 @@ module Brcobranca
           detalhe << digito_nosso_numero(pagamento.nosso_numero).to_s # digito de conferencia do nosso numero (dv)  X[01]       082 a 082
           detalhe << ''.rjust(10, '0')                                # desconto por dia                            9[10]       083 a 092
           detalhe << '2'                                              # condicao emissao boleto (2 = cliente)       9[01]       093 a 093
-          detalhe << 'N'                                              # emite boleto para debito                    X[01]       094 a 094
+          detalhe << emite_boleto_para_debito                         # emite boleto para debito                    X[01]       094 a 094
           detalhe << ''.rjust(10, ' ')                                # operacao no banco (brancos)                 X[10]       095 a 104
           detalhe << ' '                                              # indicador rateio                            X[01]       105 a 105
           detalhe << '2'                                              # endereco para aviso debito (op 2 = ignora)  9[01]       106 a 106
@@ -105,7 +106,7 @@ module Brcobranca
           detalhe << pagamento.formata_valor                          # valor do titulo                             9[13]       127 a 139
           detalhe << ''.rjust(3, '0')                                 # banco encarregado (zeros)                   9[03]       140 a 142
           detalhe << ''.rjust(5, '0')                                 # agencia depositaria (zeros)                 9[05]       143 a 147
-          detalhe << '01'                                             # especie do titulo                           9[02]       148 a 149
+          detalhe << pagamento.especie_titulo                         # especie do titulo                           9[02]       148 a 149
           detalhe << 'N'                                              # identificacao (sempre N)                    X[01]       150 a 150
           detalhe << pagamento.data_emissao.strftime('%d%m%y')        # data de emissao                             9[06]       151 a 156
           detalhe << monta_primeira_instrucao(pagamento)              # 1a instrucao                                9[02]       157 a 158
@@ -172,6 +173,12 @@ module Brcobranca
           detalhe << digito_nosso_numero(pagamento.nosso_numero).to_s    # digito de conferencia do nosso numero (dv)  X[01]       394 a 394
           detalhe << sequencial.to_s.rjust(6, '0')                       # numero do registro do arquivo               9[06]       395 a 400
           detalhe
+        end
+
+        def emite_boleto_para_debito
+          return 'N' if habilita_debito_automatico.length == 0
+
+          habilita_debito_automatico 
         end
       end
     end
