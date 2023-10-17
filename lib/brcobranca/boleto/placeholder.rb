513c11c548aeed4c41e2e3c36f5f16691d83c224
Eliel Bianchi <eliel.bianchi@rarolabs.com.br>
2023-10-17 19:19:46 -0300

    refactor: updates placeholder template

--- lib/brcobranca/boleto/placeholder.rb
+++ lib/brcobranca/boleto/placeholder.rb
@@ -4,48 +4,60 @@ module Brcobranca
   module Boleto
     class Placeholder < Base
       def initialize(campos = {})
-        campos = { carteira: '999' }.merge!(campos)
+        campos = { carteira: '' }.merge!(campos)
         super(campos)
       end
     
       def banco
-        '999'
+        ''
+      end
+
+      def logotipo
+        nil
       end
     
       def convenio=(valor)
-        @convenio = ''.rjust(5, '9')
+        @convenio = ''
       end
     
       def conta_corrente=(valor)
-        @conta_corrente = ''.rjust(5, '9')
+        @conta_corrente = ''
       end
     
       def nosso_numero=(valor)
-        @nosso_numero = ''.rjust(8, '9')
+        @nosso_numero = ''
       end
     
       def seu_numero=(valor)
-        @seu_numero = ''.rjust(7, '9')
+        @seu_numero = ''
       end
     
       def nosso_numero_dv
-        "#{agencia}#{conta_corrente}#{carteira}#{nosso_numero}".modulo10
+        ""
       end
     
       def agencia_conta_corrente_dv
-        "#{agencia}#{conta_corrente}".modulo10
+        ""
       end
     
       def nosso_numero_boleto
-        "#{carteira}/#{nosso_numero}-#{nosso_numero_dv}"
+        ""
       end
     
       def agencia_conta_boleto
-        "#{agencia} / #{conta_corrente}-#{agencia_conta_corrente_dv}"
+        ""
+      end
+
+      def codigo_barras_primeira_parte
+        "000000000000000000"
       end
     
       def codigo_barras_segunda_parte
-        "#{carteira}#{nosso_numero}#{nosso_numero_dv}#{agencia}#{conta_corrente}#{agencia_conta_corrente_dv}000"
+        "0000000000000000000000000"
+      end
+
+      def codigo_barras
+        "00000000000000000000000000000000000000000000"
       end
     end  
   end
